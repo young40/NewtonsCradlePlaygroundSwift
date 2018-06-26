ac8afa7adacf87fcc04893411141c209aa5261f5
Peter Young <young40@gmail.com>
2018-06-26 20:55:10 +0800

    update

--- NewtonsCradleiOS.playground/Contents.swift
+++ NewtonsCradleiOS.playground/Contents.swift
@@ -1,5 +1,41 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
+import PlaygroundSupport
 
-var str = "Hello, playground"
+public class NewtonsCradle: UIView {
+    private let colors: [UIColor]
+    private var balls: [UIView] = []
+    
+    private var animator: UIDynamicAnimator?
+    private var ballsToAttachmentBehaviors: [UIView:UIAttachmentBehavior] = [:]
+    private var snapBehaviors: UISnapBehavior?
+    
+    public let collisionBehavior: UICollisionBehavior
+    public let gravityBehavior: UIGravityBehavior
+    public let itemBehavior: UIDynamicItemBehavior
+    
+    public init(colors: [UIColor]) {
+        self.colors = colors
+        
+        collisionBehavior = UICollisionBehavior(items: [])
+        gravityBehavior = UIGravityBehavior(items: [])
+        itemBehavior = UIDynamicItemBehavior(items: [])
+        
+        super.init(frame: CGRect(x: 0, y: 0, width: 480, height: 320))
+        
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func draw(_ rect: CGRect) {
+        
+    }
+}
+
+let newtonsCradle = NewtonsCradle(colors: [#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1), #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)])
+
+PlaygroundPage.current.liveView = newtonsCradle
