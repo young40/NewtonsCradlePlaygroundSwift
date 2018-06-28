c730505ea1a54c08f4f5e4835f313a9284778adc
Peter Young <young40@gmail.com>
2018-06-29 00:20:15 +0800

    for OS X

--- NewtonsCradleOSX.playground/Contents.swift
+++ NewtonsCradleOSX.playground/Contents.swift
@@ -1,7 +1,7 @@
 import Cocoa
+import AppKit
 import PlaygroundSupport
 
-
-let view = NewtonsCradle()
+let view = NewtonsCradle(frame: NSRect(x: 0, y: 0, width: 320, height: 480))
 
 PlaygroundPage.current.liveView = view
--- NewtonsCradleOSX.playground/Sources/NewtonsCradle.swift
+++ NewtonsCradleOSX.playground/Sources/NewtonsCradle.swift
@@ -1,9 +1,16 @@
 import AppKit
-import Foundation
-import PlaygroundSupport
 
 public class NewtonsCradle: NSView {
+    public override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+    }
+
+    required init?(coder decoder: NSCoder) {
+        super.init(coder: decoder)
+    }
+
     public override func draw(_ dirtyRect: NSRect) {
-        
+        NSColor.gray.setFill()
+        dirtyRect.fill()
     }
 }
